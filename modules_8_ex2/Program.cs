b54d5dd937e4554a84874d4664877d42815636c1
ViFeduk <viktor2005fedchenkov@mail.ru>
2024-03-23 00:02:15 +0300

    Добавьте файлы проекта.

---
+++ modules_8_ex2/Program.cs
@@ -0,0 +1,43 @@
+﻿namespace modules_8_ex2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = @"C:\Users\Fed_w\Desktop\Webb";
+            DirectoryInfo directory = new DirectoryInfo(path);
+            long a = MemoryDirectory(directory);
+
+            Console.WriteLine("Общий вес директории: " + (a/1024/1024) +" Мегабайт");
+            long MemoryDirectory(DirectoryInfo directoryInfo)
+            {
+                long totalSize = 0;
+
+                if (directoryInfo.Exists)
+                {
+                    totalSize += MemoryFileInDerictory(directoryInfo);
+
+                    DirectoryInfo[] directories = directoryInfo.GetDirectories();
+                    foreach (DirectoryInfo d in directories)
+                    {
+                      totalSize +=MemoryDirectory(d);
+                    }
+
+                }
+                return totalSize;
+            }
+            long MemoryFileInDerictory(DirectoryInfo info)
+            {
+                long size = 0;
+                FileInfo[] fileInfo = info.GetFiles();
+                foreach (FileInfo file in fileInfo)
+                {
+                    size+= file.Length;
+                }
+                return size;
+            }
+        }
+
+
+    }
+}
